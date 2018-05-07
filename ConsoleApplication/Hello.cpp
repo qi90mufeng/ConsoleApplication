32b749873df618b5bffd6bfc22201186a1b1faad
qi90mufeng <fujin@sinafenqi.com>
2018-05-07 11:14:23 +0800

    C++ Demo

---
+++ ConsoleApplication/Hello.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+#include "hello.h"
+
+#include <iostream>
+using namespace std;
+
+int hello(){
+	//输出   ----对代码的功能做描述
+	cout << "我终于来到了喜马拉雅山" << endl;
+
+	system("pause");  //方便观察结果
+	return 0;
+}
+
---
+++ ConsoleApplication/hello.h
@@ -0,0 +1,5 @@
+#pragma once
+#ifndef A_H_  
+#define A_H_  
+int hello();
+#endif  
